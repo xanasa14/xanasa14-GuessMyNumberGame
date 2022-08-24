301ca2153f345fc1f05183d8103ca486e0d3bc61
xanasa14 <xanasa14@hotmail.com>
2022-08-24 14:40:02 -0500

    added more css and Visual Buttons options

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,46 @@
-import {View, Text} from 'react-native';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
 
 function PrimaryButton({children}){
+    function pressHandler(){
+        console.log("Pressed!!!")
+    }
     return (
-        <View>
-            <Text>
-                {children}
-            </Text>
-        </View>
+    <View style={styles.buttonOuterContainer}>
+        <Pressable 
+            onPress={pressHandler} 
+            android_ripple={{color:'644202'}}
+            style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed]: styles.buttonInnerContainer}  >
+            
+                <Text style={styles.buttonText}>
+                    {children}
+                </Text>
+        </Pressable>
+    </View>
+
 );}
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonOuterContainer:{
+        borderRadius:28,
+        margin:4, 
+        overflow:'hidden',
+        
+    },
+    buttonInnerContainer: {
+        color: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        margin:4,
+        elevation:2,
+    },
+    buttonText:{
+        color:'white',
+        textAlign:'center',
+    },
+    pressed:{
+        opacity:0.75,
+        
+    }
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen(){
 return (
     <View style={styles.inputContainer}>
-        <TextInput />
+        <TextInput 
+            style={styles.numberInput} 
+            maxLength={2}
+            keyboardType="number-pad"            
+            autoCapitalize="none"
+            autoCorrect={false}
+            />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm </PrimaryButton>
 
@@ -25,4 +31,15 @@ const styles = StyleSheet.create({
         shadowOffset:{width:1, height:2},
         shadowRadius: 0.25
     },
+    numberInput:{
+        height: 50,
+        width:50,
+        fontSize: 32,
+        borderBottomColor:'#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight:'bold',
+        textAlign:'center'
+    }
 });
