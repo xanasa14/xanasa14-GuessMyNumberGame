25ac98e99ed2e79c77275e4c527a7c470472513b
xanasa14 <xanasa14@hotmail.com>
2022-08-25 20:50:13 -0500

    greater lower logic

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import {StyleSheet,Text, View} from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
 import Title from '../components/ui/Title';
-
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 function generateRandomBetween(min, max,exclude){
     const rndNum = Math.floor(Math.random()*(max-min)) + min;
@@ -13,18 +13,36 @@ function generateRandomBetween(min, max,exclude){
         return rndNum
     }
 }
+let minBoundary = 1;
+let maxBoundary = 99;
 
 function GameScreen({userNumber}){
-    const initialGuess = generateRandomBetween(1,100,userNumber);
+    const initialGuess = generateRandomBetween(minBoundary,maxBoundary,userNumber);
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    function nextGuessHandler(direction){ // direction => lower , greater
+        if(direction ==='lower'){
+            maxBoundary = currentGuess;
+    
+        }
+        else{
+            minBoundary = currentGuess+1;
+        }
+        console.log(minBoundary,maxBoundary)
+        const newRndNumber = generateRandomBetween(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newRndNumber);
+    
+    }
+    
     return(
     <View style={styles.screen}>
         <Title style={styles.title}>Opponents Guess</Title>
             <NumberContainer>{currentGuess}</NumberContainer>
         <View>
            <Text>Higher or lower?</Text>
-        
-             {/*+ -*/} 
+           <View>
+                <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton> 
+            </View>
         </View>
         <View>{/*Log Rounds*/}</View>
     </View>
