7a3cefe6d8daf92c013a745cbf02f9753101d272
xanasa14 <xanasa14@hotmail.com>
2022-08-25 21:04:01 -0500

    Included alerts for deceiving information

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   }
   let screen = <StartGameScreen onPickedNumber={pickedNumberHandler}/>
   if (userNumber){
-    screen = <GameScreen/>
+    screen = <GameScreen userNumber={userNumber}/>
   }
   return (
     <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
